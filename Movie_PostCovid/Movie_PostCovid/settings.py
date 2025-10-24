60fa11f8125d1fd11cb162cccc881025a1319a54
soupinsong <a01052050765@gmail.com>
2025-10-24 21:00:03 +0900

    KOBIS API 연동

--- Movie_PostCovid/Movie_PostCovid/settings.py
+++ Movie_PostCovid/Movie_PostCovid/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 load_dotenv()
 
 KOBIS_KEY = os.getenv('KOBIS_API_KEY') 
-KMDB_KEY = os.getenv('KMDB_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
