15aead7b12100e4d34d05cb14c4688f926e000be
[Choichanock] <ckshrl0326@gmail.com>
2025-10-28 19:12:02 +0900

    새로운 키로 교체

--- Movie_PostCovid/Movie_PostCovid/settings.py
+++ Movie_PostCovid/Movie_PostCovid/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--a*tl0319u@r!#p2#iz%49c3!(83@p52u(#hy8h1uni7#zzf$l'
+SECRET_KEY = os.getenv('SECRET_KEY') #새로운 Secret 키 설정
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
