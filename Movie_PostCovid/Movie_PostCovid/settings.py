823d6e64f887e43344227e91b130d1be430fb1ba
[Choichanock] <ckshrl0326@gmail.com>
2025-10-29 12:00:29 +0900

    월별,제목,연도별 검색 기능과 월별 영화 랭크 차트 추가했습니ㄷ

--- Movie_PostCovid/Movie_PostCovid/settings.py
+++ Movie_PostCovid/Movie_PostCovid/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,10 +45,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'Movie_PostCovid',
     'main',
-    
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- Movie_PostCovid/main/views.py
+++ Movie_PostCovid/main/views.py
@@ -1,5 +1,7 @@
 # main/views.py
 from rest_framework import viewsets, filters
+from rest_framework.response import Response       
+from rest_framework.decorators import action   
 from .models import Movie, BoxOfficeMonthly
 from .serializers import MovieSerializer, BoxOfficeMonthlySerializer
 
@@ -9,16 +11,35 @@ class MovieViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'distributor', 'producer', 'genre']
 
+   
+    @action(detail=True, methods=['get'])
+    def rank_trend(self, request, pk=None):
+        
+        movie = self.get_object() 
+        
+        qs = BoxOfficeMonthly.objects.filter(movie=movie).order_by('year', 'month')
+        
+        data = [
+            {"month": f"{q.year}-{q.month:02}", "rank": q.rank} 
+            for q in qs
+        ]
+        return Response(data)
+   
+
 class BoxOfficeMonthlyViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = BoxOfficeMonthlySerializer
+    
     def get_queryset(self):
+        
         qs = BoxOfficeMonthly.objects.select_related('movie').order_by('year','month','rank')
         y = self.request.query_params.get('year')
         m = self.request.query_params.get('month')
         t = self.request.query_params.get('title')
         g = self.request.query_params.get('genre')
+        
         if y: qs = qs.filter(year=int(y))
         if m: qs = qs.filter(month=int(m))
         if t: qs = qs.filter(movie__title__icontains=t)
         if g: qs = qs.filter(movie__genre__icontains=g)
+        
         return qs
